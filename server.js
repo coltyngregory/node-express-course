819789c6862be6c1fc996e072e718177bb23277e
coltyngregory <coltyngregory@users.noreply.github.com>
2021-02-03 11:39:50 -0800

    add login POST route

--- server.js
+++ server.js
@@ -1,10 +1,34 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
 
 const mockUserData=[
 {name: 'Mark'},
 {name: 'Rogers'},
 ]
+
+app.post('/login', function(req, res){
+	const username = req.body.username;
+	const password = req.body.password;
+
+	const mockUsername = "Ya";
+	const mockPassword = "Yeet";
+
+	if (username===mockUsername) && password===mockPassword){
+	res.json({
+		success: true,
+		message: 'YAYEET!',
+		token: 'encrypted token goes here',
+	})
+	} else {
+		res.join({
+			success: false,
+			message: 'password and username do not match :()',
+		})
+	}
+})
 
 app.get('/users', function(req, res){
 	res.json({
@@ -14,6 +38,7 @@ app.get('/users', function(req, res){
 	})
 })
 
+//colons are used to pass variables thru url
 app.get('/users/:id',function(req,res){
 	console.log(req.params.id)
 	res.json({
