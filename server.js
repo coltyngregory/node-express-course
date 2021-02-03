7e7208e5419b7615a0ee4f3ec2e2a9dff4941e8c
coltyngregory <coltyngregory@users.noreply.github.com>
2021-02-02 19:42:21 -0800

    add first GET route

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 const express = require('express');
 const app = express();
 
+const mockUserData=[
+{name: 'Mark'},
+{name: 'Rogers'},
+]
+
+app.get('/users', function(req, res){
+	res.json({
+		success: true,
+		message: 'successfully got users. Yeet!',
+		users: mockUserData,
+	})
+})
+
 app.listen(8000, function() {
 	console.log('We have liftoff')
 })
